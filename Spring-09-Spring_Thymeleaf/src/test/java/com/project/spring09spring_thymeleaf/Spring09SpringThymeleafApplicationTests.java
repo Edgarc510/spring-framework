a7f89d51007d8a3132ae582f9b942aa7274cf322
Edgarc510 <Voltaire0709>
2022-09-13 18:21:12 -0700

    work progress module 9

---
+++ Spring-09-Spring_Thymeleaf/src/test/java/com/project/spring09spring_thymeleaf/Spring09SpringThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.spring09spring_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring09SpringThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
