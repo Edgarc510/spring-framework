c9ce427f232eaddb7354de5617374bfb63b3b8c0
Edgarc510 <Voltaire0709>
2022-11-05 12:30:00 -0700

    Annotations

--- Spring-10-ORM-TableCreation/src/main/java/com/myprojecct/entity/Student.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/myprojecct/entity/Student.java
@@ -1,14 +1,34 @@
 package com.myprojecct.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.myprojecct.enums.Gender;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
 public class Student {
 
     @Id
-    private Long Idl;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @Column(name = "studentFirstName")
     private String firstName;
+    @Column(name = "studentLastName")
     private String lastName;
     private String email;
+
+    @Transient
+    private String city;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate birthdate;
+    @Column(columnDefinition = "TIME")
+    private LocalTime birthTime;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDate birthDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
 }
---
+++ Spring-10-ORM-TableCreation/src/main/java/com/myprojecct/enums/Gender.java
@@ -0,0 +1,6 @@
+package com.myprojecct.enums;
+
+public enum Gender {
+    MALE,
+    FEMALE;
+}
