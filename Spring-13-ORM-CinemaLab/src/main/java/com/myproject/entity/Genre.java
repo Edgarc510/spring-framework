3e9d3d7f0407482cfbe87622d54e48c920b21326
Edgarc510 <Voltaire0709>
2022-11-14 16:52:01 -0800

    Created required entities

---
+++ Spring-13-ORM-CinemaLab/src/main/java/com/myproject/entity/Genre.java
@@ -0,0 +1,28 @@
+package com.myproject.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Genre extends BaseEntity{
+
+    private String name;
+
+
+    @ManyToMany
+    @JoinTable(name = "movie_genre_rel",
+    joinColumns = @JoinColumn(name = "movie_id"),
+    inverseJoinColumns = @JoinColumn(name = "genre_id"))
+
+    private List<Movie> movieList;
+
+
+}
