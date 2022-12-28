1b6b684247917033fcca9cee004ef466fe3ab390
Edgarc510 <Voltaire0709>
2022-12-28 12:54:59 -0800

    finished CourseController and course implementation

---
+++ Spring-15-REST/src/main/java/com/myproject/controller/CourseController_ResponseEntity.java
@@ -0,0 +1,12 @@
+package com.myproject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/courses/api/v2")
+public class CourseController_ResponseEntity {
+
+
+
+}
