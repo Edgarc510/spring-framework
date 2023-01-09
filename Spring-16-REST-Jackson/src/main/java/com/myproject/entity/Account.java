833bb241c3f5de70f7b09a3a82ad87f80a55e3ab
Edgarc510 <Voltaire0709>
2023-01-09 12:35:12 -0800

    Added additional entities and repositories

--- Spring-16-REST-Jackson/src/main/java/com/myproject/entity/Account.java
+++ Spring-16-REST-Jackson/src/main/java/com/myproject/entity/Account.java
@@ -3,6 +3,7 @@ package com.myproject.entity;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.myproject.enums.UserRole;
 import lombok.*;
 
 
@@ -13,7 +14,7 @@ import javax.persistence.*;
 @Setter
 @NoArgsConstructor
 @Table(name = "account_details")
-@JsonIgnoreProperties(value = {"hibernateLazyInitializer"},ignoreUnknown = true)
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer"},ignoreUnknown = true)//ignoreUnknown will ignore anything not included in the class fields, security related
 public class Account extends BaseEntity{
 
     private String name;
@@ -26,11 +27,11 @@ public class Account extends BaseEntity{
     @Column(name = "postal_code")
     private String postalCode;
 
-//    @Enumerated(EnumType.STRING)
-//    private UserRole role = UserRole.USER;
-//
-//    @OneToOne(mappedBy = "account")
-//    @JsonBackReference //is the back part of reference - it will be omitted from serialization
-//    private User user;
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @OneToOne(mappedBy = "account")
+    @JsonBackReference //is the back part of reference - it will be omitted from serialization
+    private User user;
 
 }
--- Spring-16-REST-Jackson/src/main/java/com/myproject/entity/BaseEntity.java
+++ Spring-16-REST-Jackson/src/main/java/com/myproject/entity/BaseEntity.java
@@ -1,6 +1,8 @@
 package com.myproject.entity;
 
-import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
