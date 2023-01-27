735eab0a6f8a32907bb8c1e3e73696dbfcd9184e
Edgarc510 <Voltaire0709>
2023-01-27 11:37:09 -0800

    first commit aop

---
+++ Spring-20-AOP/src/main/java/com/myproject/repository/CourseRepository.java
@@ -0,0 +1,12 @@
+package com.myproject.repository;
+
+import com.myproject.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findAllByCategory(String category);
+
+}
