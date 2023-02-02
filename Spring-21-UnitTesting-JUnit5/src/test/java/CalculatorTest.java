e9e1e81d9cf34ebaca84ab8e60c87057af651684
Edgarc510 <Voltaire0709>
2023-02-01 20:42:03 -0800

    initial commit for unit testing and fixed pom files

---
+++ Spring-21-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+
+    @Test
+    void testCase1(){
+        fail("not implemented yet");
+    }
+
+    @Test
+    void testCase2(){
+        fail("not implemented yet");
+    }
+
+    @Test
+    void testCase3(){
+        fail("not implemented yet");
+    }
+
+    @Test
+    void testCase4(){
+
+    }
+
+    @Test
+    void testCase5(){
+
+    }
+
+    @Test
+    void add(){
+        int actual = Calculator.add(2,3);
+        System.out.println("actual = " + actual);
+    }
+
+}
