cd3efd48b4fcaf2ce8299cb39e5a8f6f18e5c05b
Edgarc510 <Voltaire0709>
2023-02-03 09:45:09 -0800

    added more unit tests

---
+++ Spring-23-UnitTesting-MockMvc/src/test/java/com/myproject/controller/StudentControllerTest.java
@@ -0,0 +1,71 @@
+package com.myproject.controller;
+
+import com.myproject.entity.Student;
+import com.myproject.service.StudentService;
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(StudentController.class)
+class StudentControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    StudentService studentService;
+
+
+    @Test
+    void getStudent() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders
+                        .get("/student")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("{\"id\": 0, \"firstName\": \"Mike\", \"lastName\": \"Smith\", \"age\": 20}"))
+                .andReturn();
+
+    }
+
+    @Test
+    void jsonAssert() throws JSONException {
+
+        String expected = "{\"id\": 0, \"firstName\": \"Mike\", \"lastName\": \"Smith\"}";
+        String actual = "{\"id\": 0, \"firstName\": \"Mike\", \"lastName\": \"Smith\", \"age\": 20}";
+
+        JSONAssert.assertEquals(expected, actual, false);
+
+    }
+
+    @Test
+    void getStudent_service() throws Exception {
+
+        when(studentService.getStudent()).thenReturn(Arrays.asList(
+                new Student("John", "Doe", 20),
+                new Student("Tom", "Hanks", 50)
+        ));
+
+        mvc.perform(MockMvcRequestBuilders
+                        .get("/service/student")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.content().json("[{\"id\": 0, \"firstName\": \"John\", \"lastName\": \"Doe\", \"age\": 20},{\"id\": 0, \"firstName\": \"Tom\", \"lastName\": \"Hanks\", \"age\": 50}]"))
+                .andReturn();
+
+    }
+}
